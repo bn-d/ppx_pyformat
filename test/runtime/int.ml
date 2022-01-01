f9826fb0e70018d51c43d6da0820c62e81f3993f
Boning <me@boni.ng>
2022-01-01 03:35:54 +0000

    Add octal test

--- test/runtime/int.ml
+++ test/runtime/int.ml
@@ -166,4 +166,78 @@ let suite =
                       (int_to_decimal ~fill:(Center, '.', 11) ~sign:Plus
                          ~grouping_option:Comma 1000);
               ];
+         "octal"
+         >::: [
+                "simple_1" >:: test "0" (int_to_octal 0);
+                "simple_2" >:: test "1" (int_to_octal 1);
+                "simple_3" >:: test "173" (int_to_octal 123);
+                "simple_4" >:: test "-173" (int_to_octal (-123));
+                "alternate_form_1"
+                >:: test "0o1" (int_to_octal ~alternate_form:true 1);
+                "alternate_form_2"
+                >:: test "-0o1" (int_to_octal ~alternate_form:true (-1));
+                "sign"
+                >::: [
+                       "plus_1" >:: test "+1" (int_to_octal ~sign:Plus 1);
+                       "plus_2" >:: test "-1" (int_to_octal ~sign:Plus (-1));
+                       "plus_3" >:: test "+0" (int_to_octal ~sign:Plus 0);
+                       "minus_1" >:: test "1" (int_to_octal ~sign:Minus 1);
+                       "minus_2" >:: test "-1" (int_to_octal ~sign:Minus (-1));
+                       "minus_3" >:: test "0" (int_to_octal ~sign:Minus 0);
+                       "space_1" >:: test " 1" (int_to_octal ~sign:Space 1);
+                       "space_2" >:: test "-1" (int_to_octal ~sign:Space (-1));
+                       "space_3" >:: test " 0" (int_to_octal ~sign:Space 0);
+                     ];
+                "grouping_1" >:: test "0" (int_to_octal ~grouping:true 0);
+                "grouping_2" >:: test "1000" (int_to_octal ~grouping:true 512);
+                "grouping_3"
+                >:: test "1_0000" (int_to_octal ~grouping:true 4096);
+                "grouping_4"
+                >:: test "100_0000" (int_to_octal ~grouping:true 262144);
+                "grouping_5"
+                >:: test "1000_0000" (int_to_octal ~grouping:true 2097152);
+                "grouping_6"
+                >:: test "1_0000_0000" (int_to_octal ~grouping:true 16777216);
+                "align"
+                >::: [
+                       "left_1"
+                       >:: test "1...." (int_to_octal ~fill:(Left, '.', 5) 1);
+                       "left_2"
+                       >:: test "-1..." (int_to_octal ~fill:(Left, '.', 5) (-1));
+                       "right_1"
+                       >:: test "....1" (int_to_octal ~fill:(Right, '.', 5) 1);
+                       "right_2"
+                       >:: test "...-1"
+                             (int_to_octal ~fill:(Right, '.', 5) (-1));
+                       "center_1"
+                       >:: test "..1.." (int_to_octal ~fill:(Center, '.', 5) 1);
+                       "center_2"
+                       >:: test ".-1.."
+                             (int_to_octal ~fill:(Center, '.', 5) (-1));
+                       "pad_1"
+                       >:: test "....1" (int_to_octal ~fill:(Pad, '.', 5) 1);
+                       "pad_1"
+                       >:: test "-...1" (int_to_octal ~fill:(Pad, '.', 5) (-1));
+                     ];
+                "complex_1"
+                >:: test "+0o0_0001"
+                      (int_to_octal ~fill:(Pad, '0', 9) ~sign:Plus
+                         ~grouping:true ~alternate_form:true 1);
+                "complex_2"
+                >:: test " 0o0_0001"
+                      (int_to_octal ~fill:(Pad, '0', 8) ~sign:Space
+                         ~grouping:true ~alternate_form:true 1);
+                "complex_3"
+                >:: test "-0o.....1"
+                      (int_to_octal ~fill:(Pad, '.', 9) ~sign:Minus
+                         ~grouping:true ~alternate_form:true (-1));
+                "complex_4"
+                >:: test "0o....1"
+                      (int_to_octal ~fill:(Pad, '.', 7) ~sign:Minus
+                         ~grouping:true ~alternate_form:true 1);
+                "complex_5"
+                >:: test "..+0o1..."
+                      (int_to_octal ~fill:(Center, '.', 9) ~sign:Plus
+                         ~grouping:true ~alternate_form:true 1);
+              ];
        ]
