fa3349fe2b3b483eb0e8a0520ed77333d26bfc6d
Boning <me@boni.ng>
2022-01-02 19:23:29 +0000

    Add impl and test for string

--- src/element_gen.ml
+++ src/element_gen.ml
@@ -20,8 +20,37 @@ let apply_conversion ~loc conversion (expr : P.expression) : P.expression =
       let open P in
       [%expr [%e fun_expr] [%e expr]]
 
-let apply_format_spec format_spec ~loc:_ (_expr : P.expression) : P.expression =
-  match format_spec with _ -> failwith "not impl"
+let apply_align ~loc func_lid c w (expr : P.expression) : P.expression =
+  let func_expr = Ast_helper.Exp.ident ~loc P.{ txt = func_lid; loc } in
+  let c_expr = Ast_builder.echar ~loc (Option.value ~default:' ' c) in
+  let w_expr = Ast_builder.eint ~loc w in
+  let open P in
+  [%expr [%e func_expr] [%e c_expr] [%e w_expr] [%e expr]]
+
+let apply_left_align =
+  apply_align (Longident.Ldot (Lident "Ppx_pyformat_runtime", "align_left"))
+
+let apply_right_align =
+  apply_align (Longident.Ldot (Lident "Ppx_pyformat_runtime", "align_right"))
+
+let apply_center_align =
+  apply_align (Longident.Ldot (Lident "Ppx_pyformat_runtime", "align_center"))
+
+let apply_string_format ~loc ~fill (expr : P.expression) : P.expression =
+  match fill with
+  | None | Some ({ align = Pad; _ }, _) -> expr
+  | Some ({ align = Left; char_ }, w) -> apply_left_align ~loc char_ w expr
+  | Some ({ align = Right; char_ }, w) -> apply_right_align ~loc char_ w expr
+  | Some ({ align = Center; char_ }, w) -> apply_center_align ~loc char_ w expr
+
+let apply_format_spec ~loc format_spec (expr : P.expression) : P.expression =
+  (* all the validation should be done in type_utils, so do not check
+   * unused fields here *)
+  match format_spec with
+  | String_format { fill } -> apply_string_format ~loc ~fill expr
+  | _ -> failwith "not impl"
+(*| Int_format spec -> apply_int_format ~loc spec expr
+  | Float_format spec -> apply_float_format ~loc spec expr*)
 
 let string_expr_of_rfield ~loc (rfield : replacement_field) : P.expression =
   (match rfield.arg with
--- test/rewriter/test_pyformat_rewriter.ml
+++ test/rewriter/test_pyformat_rewriter.ml
@@ -5,11 +5,64 @@ let test exp act _ = assert_equal ~printer:Fun.id exp act
 let text_tests =
   "text"
   >::: [
-         "simple" >:: test [%pyformat "123abcABC,.;' \t\n"] "123abcABC,.;' \t\n";
-         "curl_escape" >:: test [%pyformat "{{}}}}{{"] "{}}{";
-         "consolidation" >:: test [%pyformat "123{{}}321"] "123{}321";
+         "simple" >:: test "123abcABC,.;' \t\n" [%pyformat "123abcABC,.;' \t\n"];
+         "curl_escape" >:: test "{}}{" [%pyformat "{{}}}}{{"];
+         "consolidation" >:: test "123{}321" [%pyformat "123{{}}321"];
        ]
 
-let suite = "test_pyformat_rewriter" >::: [ text_tests ]
+let index_tests =
+  "index"
+  >::: [
+         "simple_1"
+         >:: test "123"
+               (let l = [ "123" ] in
+                [%pyformat "{l[0]}"]);
+         "simple_2"
+         >:: test "456"
+               (let l = [ ""; "456" ] in
+                [%pyformat "{l[1]}"]);
+       ]
+
+let conversion_tests =
+  "conversion"
+  >::: [
+         "simple_1"
+         >:: test "1."
+               (let f = 1.0 in
+                [%pyformat "{f!string_of_float}"]);
+         "simple_2"
+         >:: test "1."
+               (let f = 1.0 in
+                [%pyformat "{f!Float.to_string}"]);
+       ]
+
+let string_tests =
+  "string"
+  >::: [
+         "simple"
+         >:: test "str"
+               (let s = "str" in
+                [%pyformat "{s}"]);
+         "left"
+         >:: test "str__"
+               (let s = "str" in
+                [%pyformat "{s:_<5}"]);
+         "right"
+         >:: test "__str"
+               (let s = "str" in
+                [%pyformat "{s:_>5}"]);
+         "center"
+         >:: test "_str_"
+               (let s = "str" in
+                [%pyformat "{s:_^5}"]);
+         "complex"
+         >:: test "_str_"
+               (let l = [ "str" ] in
+                [%pyformat "{l[0]!Fun.id:_^5}"]);
+       ]
+
+let suite =
+  "test_pyformat_rewriter"
+  >::: [ text_tests; index_tests; conversion_tests; string_tests ]
 
 let _ = run_test_tt_main suite
